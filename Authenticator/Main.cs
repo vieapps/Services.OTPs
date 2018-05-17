449e91b5fa83e2d59b4cf5322e744e1ba8d55cdd
Quynh Nguyen <mr.quynh@gmail.com>
2018-05-17 19:03:36 +0700

    Refactoring

---
+++ Authenticator/Main.cs
@@ -0,0 +1,93 @@
+﻿#region Related components
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+using Newtonsoft.Json.Linq;
+using net.vieapps.Components.Utility;
+#endregion
+
+namespace net.vieapps.Services.OTPs
+{
+	public class ServiceComponent : ServiceBase
+	{
+		public ServiceComponent() : base() { }
+
+		public override void Start(string[] args = null, bool initializeRepository = true, Func<IService, Task> next = null) => base.Start(args, false, next);
+
+		internal string AuthenticationKey => this.GetKey("Authentication", "VIEApps-65E47754-NGX-50C0-Services-4565-Authentication-BA55-Key-A8CC23879C5D");
+
+		public override string ServiceName => "AuthenticatorOTP";
+
+		public override async Task<JObject> ProcessRequestAsync(RequestInfo requestInfo, CancellationToken cancellationToken = default(CancellationToken))
+		{
+			try
+			{
+				return requestInfo.Verb.Equals("GET")
+					? await UtilityService.ExecuteTask(() => this.ProcessOtpRequest(requestInfo), cancellationToken).ConfigureAwait(false)
+					: throw new MethodNotAllowedException(requestInfo.Verb);
+			}
+			catch (Exception ex)
+			{
+				throw this.GetRuntimeException(requestInfo, ex);
+			}
+		}
+
+		JObject ProcessOtpRequest(RequestInfo requestInfo, CancellationToken cancellationToken = default(CancellationToken))
+		{
+			// check
+			if (requestInfo.Extra == null)
+				throw new InvalidRequestException();
+
+			// prepare
+			var id = requestInfo.Extra.ContainsKey("ID") ? requestInfo.Extra["ID"].Decrypt(this.EncryptionKey) : "";
+			var stamp = requestInfo.Extra.ContainsKey("Stamp") ? requestInfo.Extra["Stamp"].Decrypt(this.EncryptionKey) : "";
+
+			var key = (id + "@" + stamp).ToLower().GetHMACHash(this.AuthenticationKey.ToBytes(), "SHA512");
+			var response = new JObject();
+
+			// setup for provisioning
+			if (requestInfo.Extra.ContainsKey("Setup"))
+			{
+				var account = requestInfo.Extra.ContainsKey("Account") ? requestInfo.Extra["Account"].Decrypt(this.EncryptionKey) : "";
+				var issuer = requestInfo.Extra.ContainsKey("Issuer") ? requestInfo.Extra["Issuer"].Decrypt(this.EncryptionKey) : "";
+				if (string.IsNullOrWhiteSpace(issuer))
+					issuer = UtilityService.GetAppSetting("OTPs:Issuer", "VIEApps NGX");
+				var size = requestInfo.Extra.ContainsKey("Size") ? requestInfo.Extra["Size"].CastAs<int>() : UtilityService.GetAppSetting("OTPs:QRCode-Size", "300").CastAs<int>();
+				var ecl = requestInfo.Extra.ContainsKey("ECCLevel") ? requestInfo.Extra["ECCLevel"] : UtilityService.GetAppSetting("OTPs:QRCode-ECCLevel", "L");
+				var provisioningUri = OTPService.GenerateProvisioningUri(account, key, issuer.UrlEncode());
+				var imageUri = this.GetHttpURI("Files", "https://afs.vieapps.net")
+					+ "/qrcodes/" + UtilityService.NewUUID.Encrypt(null, true).Substring(UtilityService.GetRandomNumber(13, 43), 13)
+					+ "?v=" + provisioningUri.Encrypt(this.EncryptionKey).ToBase64Url(true)
+					+ "&t=" + DateTime.Now.ToUnixTimestamp().ToString().Encrypt(this.EncryptionKey).ToBase64Url(true)
+					+ "&s=" + size + "&ecl=" + ecl;
+				response.Add("Uri", new JValue(imageUri));
+			}
+
+			// validate input of client
+			else
+			{
+				var password = requestInfo.Extra.ContainsKey("Password") ? requestInfo.Extra["Password"].Decrypt(this.EncryptionKey) : "";
+				if (string.IsNullOrWhiteSpace(password))
+					throw new OTPLoginFailedException();
+
+				var interval = "App".IsEquals(requestInfo.Extra.ContainsKey("Type") ? requestInfo.Extra["Type"] : "App") ? 30 : 300;
+				if (!password.Equals(OTPService.GeneratePassword(key, interval)))
+					throw new OTPLoginFailedException();
+			}
+
+			// response
+			return response;
+		}
+
+	}
+
+	[Serializable]
+	public class OTPLoginFailedException : AppException
+	{
+		public OTPLoginFailedException() : base("Bad OTP") { }
+		public OTPLoginFailedException(string message) : base(message) { }
+		public OTPLoginFailedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+	}
+}
--- Vasco/Main.cs
+++ Vasco/Main.cs
@@ -1,16 +1,11 @@
 ﻿#region Related components
 using System;
-using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
-using System.Collections.Generic;
 
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-
 using net.vieapps.Components.Utility;
-
 using IdentikeyAuthWrapper.vasco.identikey.model;
 using IdentikeyAuthWrapper.vasco.identikey.authentication;
 #endregion
@@ -19,108 +14,27 @@ namespace net.vieapps.Services.OTPs
 {
 	public class ServiceComponent : ServiceBase
 	{
-
-		#region Start
 		public ServiceComponent() : base() { }
 
-		public override void Start(string[] args = null, bool initializeRepository = true, Func<IService, Task> next = null)
-		{
-			base.Start(args, false, next);
-		}
+		public override void Start(string[] args = null, bool initializeRepository = true, Func<IService, Task> next = null) => base.Start(args, false, next);
 
-		internal string AuthenticationKey
-		{
-			get
-			{
-				return this.GetKey("Authentication", "VIEApps-65E47754-NGX-50C0-Services-4565-Authentication-BA55-Key-A8CC23879C5D");
-			}
-		}
-
-		public override string ServiceName { get { return "OTPs"; } }
-		#endregion
+		public override string ServiceName => "VascoOTP";
 
 		public override async Task<JObject> ProcessRequestAsync(RequestInfo requestInfo, CancellationToken cancellationToken = default(CancellationToken))
 		{
-			// check
-			if (!requestInfo.Verb.Equals("GET"))
-				throw new MethodNotAllowedException(requestInfo.Verb);
-
-			// process
 			try
 			{
-				switch (requestInfo.ObjectName.Trim().ToLower())
-				{
-					case "authenticator":
-						return await UtilityService.ExecuteTask(() => this.ProcessAuthenticatorOtpRequest(requestInfo), cancellationToken).ConfigureAwait(false);
-
-					case "vasco":
-						return await UtilityService.ExecuteTask(() => this.ProcessVascoOtpRequest(requestInfo), cancellationToken).ConfigureAwait(false);
-
-					default:
-						throw new InvalidRequestException($"The request is invalid [({requestInfo.Verb}): {requestInfo.URI}]");
-				}
+				return requestInfo.Verb.Equals("GET")
+					? await UtilityService.ExecuteTask(() => this.ProcessOtpRequest(requestInfo), cancellationToken).ConfigureAwait(false)
+					: throw new MethodNotAllowedException(requestInfo.Verb);
 			}
 			catch (Exception ex)
 			{
-				await this.WriteLogAsync(requestInfo.CorrelationID, "Error occurred while processing", ex).ConfigureAwait(false);
 				throw this.GetRuntimeException(requestInfo, ex);
 			}
 		}
 
-		#region Process one-time password of Authenticator
-		JObject ProcessAuthenticatorOtpRequest(RequestInfo requestInfo, CancellationToken cancellationToken = default(CancellationToken))
-		{
-			// check
-			if (requestInfo.Extra == null)
-				throw new InvalidRequestException();
-
-			// prepare
-			var id = requestInfo.Extra.ContainsKey("ID") ? requestInfo.Extra["ID"].Decrypt(this.EncryptionKey) : "";
-			var stamp = requestInfo.Extra.ContainsKey("Stamp") ? requestInfo.Extra["Stamp"].Decrypt(this.EncryptionKey) : "";
-
-			var key = (id + "@" + stamp).ToLower().GetHMACHash(this.AuthenticationKey.ToBytes(), "SHA512");
-			var response = new JObject();
-
-			// setup for provisioning
-			if (requestInfo.Extra.ContainsKey("Setup"))
-			{
-				var account = requestInfo.Extra.ContainsKey("Account") ? requestInfo.Extra["Account"].Decrypt(this.EncryptionKey) : "";
-				var issuer = requestInfo.Extra.ContainsKey("Issuer") ? requestInfo.Extra["Issuer"].Decrypt(this.EncryptionKey) : "";
-				if (string.IsNullOrWhiteSpace(issuer))
-					issuer = UtilityService.GetAppSetting("OTPs:Issuer", "VIE Apps NGX");
-				var size = requestInfo.Extra.ContainsKey("Size") ? requestInfo.Extra["Size"].CastAs<int>() : UtilityService.GetAppSetting("OTPs:QRCode-Size", "300").CastAs<int>();
-				var ecl = requestInfo.Extra.ContainsKey("ECCLevel") ? requestInfo.Extra["ECCLevel"] : UtilityService.GetAppSetting("OTPs:QRCode-ECCLevel", "L");
-				var provisioningUri = OTPService.GenerateProvisioningUri(account, key, issuer.UrlEncode());
-				var imageUri = this.GetHttpURI("Files", "https://afs.vieapps.net")
-					+ "/qrcodes/" + UtilityService.NewUID.Encrypt().ToHexa(true).Substring(UtilityService.GetRandomNumber(13, 43), 13)
-					+ "?v=" + provisioningUri.Encrypt(this.EncryptionKey).ToBase64Url(true)
-					+ "&t=" + DateTime.Now.ToUnixTimestamp().ToString().Encrypt(this.EncryptionKey).ToBase64Url(true)
-					+ "&s=" + size + "&ecl=" + ecl;
-				response = new JObject()
-				{
-					{ "Uri", imageUri }
-				};
-			}
-
-			// validate input of client
-			else
-			{
-				var password = requestInfo.Extra.ContainsKey("Password") ? requestInfo.Extra["Password"].Decrypt(this.EncryptionKey) : "";
-				if (string.IsNullOrWhiteSpace(password))
-					throw new OTPLoginFailedException();
-
-				var interval = (requestInfo.Extra.ContainsKey("Type") ? requestInfo.Extra["Type"] : "App").IsEquals("App") ? 30 : 300;
-				if (!password.Equals(OTPService.GeneratePassword(key, interval)))
-					throw new OTPLoginFailedException();
-			}
-
-			// response
-			return response;
-		}
-		#endregion
-
-		#region Process one-time-password of VASCO Identity
-		JObject ProcessVascoOtpRequest(RequestInfo requestInfo)
+		JObject ProcessOtpRequest(RequestInfo requestInfo)
 		{
 			// prepare
 			var domain = "";
@@ -194,8 +108,6 @@ namespace net.vieapps.Services.OTPs
 
 			return new JObject();
 		}
-		#endregion
-
 	}
 
 	#region Exceptions
